9d01e98e9b08d69715729e1860621e93db1f5bff
MFMvanderholst <117632371+MFMvanderholst@users.noreply.github.com>
2024-02-07 13:46:17 +0100

    made rolls

---
+++ windkracht12/database/seeders/UserSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        /**
+         * states when the seeder begins
+         * puts a message on the terminal with Updating new seed ...
+         */
+        $this->command->info('Updating new seed ...');
+        /**
+         * makes an admin account 
+         */
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => 'password',
+            'admin_roll' => 1
+        ]);
+        /**
+         * makes an instructor account 
+         */
+        User::create([
+            'name' => 'Instructor',
+            'email' => 'instructor@gmail.com',
+            'email_verified_at' => now(),
+            'password' => 'password',
+            'instructor_roll' => 1
+        ]);
+        /**
+         * states when the seeder is done 
+         * puts a message on the terminal with Users completed message
+         */
+        $this->command->info('Users completed');
+    }
+}
