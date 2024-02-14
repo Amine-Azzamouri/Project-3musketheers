193ce6b9fad5d512e6eafec03c49a3ab84d8a37c
MFMvanderholst <117632371+MFMvanderholst@users.noreply.github.com>
2024-02-14 14:20:38 +0100

    mail verify done

--- windkracht12/resources/views/welcome.blade.php
+++ windkracht12/resources/views/welcome.blade.php
@@ -23,10 +23,10 @@
                     @auth
                         <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
                     @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+                        <!-- <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a> -->
 
                          <!-- Trigger/Open The Modal -->
-                         <x-secondary-button id="myBtn">Login</x-secondary-button>
+                         <x-primary-button id="myBtn">Login</x-primary-button>
 
                         <!-- The Modal -->
                         <div id="myModal" class="modal" style="display:none">
